fbd7215ac7f00c44506245ccce3c564dec0bce42
nomvula <nzshabangu95@gmail.com>
2023-09-08 11:46:19 +0200

    Add project files.

---
+++ TryCatchErrorHandling/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnNum_Click(sender As Object, e As EventArgs) Handles btnNum.Click
+        Dim value As String
+        value = txtNum.Text
+
+        Try
+            Dim num As Double
+            num = CDbl(value)
+
+            MsgBox("you entered a value : " & num)
+        Catch
+            MsgBox("Error: " & value & " Is not a number")
+        End Try
+    End Sub
+End Class
